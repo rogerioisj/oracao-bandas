217d56c68052c789aac8204a806ced795146fdcf
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2024-01-13 15:35:14 -0300

    feat: add update dto

--- src/modules/bands/structs/bandsDto.go
+++ src/modules/bands/structs/bandsDto.go
@@ -1,9 +1,17 @@
 package structs
 
-type BandDto struct {
+type SaveBandDto struct {
 	Title   string `json:"title" binding:"required"`
 	Type    string `json:"type" binding:"required"`
 	Country string `json:"country" binding:"required"`
 	UF      string `json:"uf"`
 	Style   string `json:"style" binding:"required"`
+}
+
+type UpdateBandDto struct {
+	Title   string `json:"title"`
+	Type    string `json:"type"`
+	Country string `json:"country"`
+	UF      string `json:"uf"`
+	Style   string `json:"style"`
 }
