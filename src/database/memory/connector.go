511e98005cee7924df4353d285494dd3c722008f
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2024-01-15 07:43:41 -0300

    feat: add connector for in memory db

---
+++ src/database/memory/connector.go
@@ -0,0 +1,18 @@
+package memory
+
+import (
+	"log"
+	"oracao-bandas.com/src/database/resources/bands/entities"
+)
+
+type DB struct {
+	Bands []entities.Band
+}
+
+func Connector() *DB {
+	log.Print("Success to initialize database connection. On memory database connected.")
+
+	var db DB
+
+	return &db
+}
