84928618a613a0910f63fa024729cea53aa556c3
Rogerio Inacio <rogerio.inacio@u4crypto.com>
2024-01-15 07:44:33 -0300

    feat: init database

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "oracao-bandas.com/src/server"
+import (
+	"oracao-bandas.com/src/database/orm"
+	"oracao-bandas.com/src/database/postgres"
+	"oracao-bandas.com/src/server"
+)
 
 func main() {
+	db := postgres.Connect()
+
+	orm.LoadEntities(db)
+
 	server.StartServer()
 }
