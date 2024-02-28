8fc949c257629ab5e069b7b4413ae015e9d6e483
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-02-28 20:11:42 -0300

    feat: add behavior to load templates

--- src/server/server.go
+++ src/server/server.go
@@ -2,24 +2,23 @@ package server
 
 import (
 	"github.com/gin-gonic/gin"
-	"net/http"
-	"oracao-bandas.com/src/modules/bands"
-	"oracao-bandas.com/src/modules/home"
+	"oracao-bandas.com/src/api/modules/bands"
+	"oracao-bandas.com/src/api/modules/home"
+	"oracao-bandas.com/src/controllers"
 )
 
 func StartServer() {
 	router := gin.Default()
+
+	router.LoadHTMLGlob("src/views/**/*")
+	router.Static("/static", "src/static/")
 
 	api := router.Group("api")
 
 	home.SetupRoutes(api)
 	bands.SetupRoutes(api)
 
-	api.GET("/test", func(context *gin.Context) {
-		context.JSON(http.StatusOK, gin.H{
-			"message": "ok",
-		})
-	})
+	router.GET("/", controllers.Home)
 
 	err := router.Run(":8000")
 	if err != nil {
