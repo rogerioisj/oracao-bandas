36005d0b93e718b7aa847b0949c5d527cf6f7368
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-02-28 22:55:54 -0300

    refactor: use config value instead hardcoded info

--- src/server/server.go
+++ src/server/server.go
@@ -4,10 +4,11 @@ import (
 	"github.com/gin-gonic/gin"
 	"oracao-bandas.com/src/api/modules/bands"
 	"oracao-bandas.com/src/api/modules/home"
+	"oracao-bandas.com/src/configuration"
 	"oracao-bandas.com/src/controllers"
 )
 
-func StartServer() {
+func StartServer(config *configuration.Configuration) {
 	router := gin.Default()
 
 	router.LoadHTMLGlob("src/views/**/*")
@@ -20,7 +21,7 @@ func StartServer() {
 
 	router.GET("/", controllers.Home)
 
-	err := router.Run(":8000")
+	err := router.Run(config.System.Port)
 	if err != nil {
 		return
 	}
