50847a80b75979c6141fc56a1b1bf92b2e2ba0a9
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-02-29 12:07:43 -0300

    refactor: config service to implement dependency injection

--- src/api/modules/bands/services/saveBand.go
+++ src/api/modules/bands/services/saveBand.go
@@ -3,9 +3,24 @@ package services
 import (
 	"oracao-bandas.com/src/api/modules/bands/structs"
 	"oracao-bandas.com/src/database/resources/bands/entities"
+	"oracao-bandas.com/src/database/resources/bands/repositories"
 )
 
-func SaveBand(band *structs.SaveBandDto) (error, entities.Band) {
+type SaveBandServiceInterface interface {
+	SaveBand(band *structs.SaveBandDto) (error, entities.Band)
+}
+
+type SaveBandService struct {
+	repository repositories.BandRepositoryInterface
+}
+
+func NewSaveBandService(repository repositories.BandRepositoryInterface) *SaveBandService {
+	return &SaveBandService{
+		repository: repository,
+	}
+}
+
+func (service *SaveBandService) SaveBand(band *structs.SaveBandDto) (error, entities.Band) {
 	var savedBand entities.Band
 
 	savedBand.Title = band.Title
@@ -21,6 +36,12 @@ func SaveBand(band *structs.SaveBandDto) (error, entities.Band) {
 	if response.Error != nil {
 		return response.Error, entities.Band{}
 	}*/
+
+	err := service.repository.Save(&savedBand)
+
+	if err != nil {
+		return err, entities.Band{}
+	}
 
 	return nil, savedBand
 }
