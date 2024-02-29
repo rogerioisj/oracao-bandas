54cc067c8b943ac59ecfa64b3f8d80a72c580501
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-02-28 22:27:29 -0300

    refactor: load db info from config file

--- src/database/postgres/connector.go
+++ src/database/postgres/connector.go
@@ -1,20 +1,24 @@
 package postgres
 
 import (
+	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 	"log"
+	"oracao-bandas.com/src/configuration"
 )
 
-func Connect() *gorm.DB {
-	dsn := "'"
+func Connect(config *configuration.Configuration) *gorm.DB {
+	dsn := fmt.Sprintf("host='%s' user='%s' password='%s' dbname='%s' port='%d' sslmode=disable",
+		config.Database.Host, config.Database.User, config.Database.Password, config.Database.DBName, config.Database.Port)
+
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
 	if err != nil {
 		log.Printf("Fail to connect to database: %s", err)
 	}
 
-	log.Printf("Using Postgres database at port 5432 and Bands database")
+	log.Printf("Using Postgres database at port %v", config.Database.Port)
 
 	db.Exec(`CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`)
 
