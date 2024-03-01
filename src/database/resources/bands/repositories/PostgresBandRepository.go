8698342bdfb5aaa510ef5d7ff6a5d79fa48492ab
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-03-01 17:45:21 -0300

    feat: add function to load bands

--- src/database/resources/bands/repositories/PostgresBandRepository.go
+++ src/database/resources/bands/repositories/PostgresBandRepository.go
@@ -32,6 +32,25 @@ func (repository *PostgresBandRepository) Get(id string) {
 
 }
 
-func (repository *PostgresBandRepository) List(page int, number int) {
+func (repository *PostgresBandRepository) List(page int, number int) ([]entities.Band, error) {
+	log.Printf("Listing bands for page: %v and number: %v", page, number)
 
+	var bands []entities.Band
+
+	var offset int
+
+	if page < 1 {
+		offset = 0
+	} else {
+		offset = (page - 1) * number
+	}
+
+	response := repository.db.Order("created_at desc").Order("title").Offset(offset).Limit(number).Find(&bands)
+
+	if response.Error != nil {
+		log.Printf("Error to load Bands. Details: %s", response.Error)
+		return nil, response.Error
+	}
+
+	return bands, nil
 }
