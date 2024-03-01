7090bd43d3fbd81cb69b37a65e69cef44a417d7f
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-03-01 17:45:03 -0300

    refactor: change return type

--- src/database/resources/bands/repositories/bandRepositoryInterface.go
+++ src/database/resources/bands/repositories/bandRepositoryInterface.go
@@ -9,5 +9,5 @@ type BandRepositoryInterface interface {
 	Save(dto *entities.Band) error
 	Update(dto *structs.UpdateBandDto)
 	Get(id string)
-	List(page int, number int)
+	List(page int, number int) ([]entities.Band, error)
 }
