9c241ef37b4942b0fc64fd0456f441b5ece6a12b
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-03-03 13:05:05 -0300

    feat: add function greater than to checker array length

--- src/server/server.go
+++ src/server/server.go
@@ -5,6 +5,7 @@ import (
 	"gorm.io/gorm"
 	"html/template"
 	"oracao-bandas.com/src/configuration"
+	"oracao-bandas.com/src/database/resources/bands/entities"
 	"oracao-bandas.com/src/modules"
 )
 
@@ -15,6 +16,7 @@ func StartServer(config *configuration.Configuration, db *gorm.DB) {
 		"isEven": func(value int) bool {
 			return value%2 == 0
 		},
+		"greaterThan": func(array []entities.Band, value int) bool { return len(array) > value },
 	})
 
 	router.LoadHTMLGlob("src/views/**/*")
