61cdb2981d636f91abf691805fd3c4f8eb8f18ea
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-03-04 00:12:09 -0300

    feat: import band service+

--- src/modules/home/home.module.go
+++ src/modules/home/home.module.go
@@ -2,15 +2,11 @@ package home
 
 import (
 	"github.com/gin-gonic/gin"
-	"gorm.io/gorm"
-	"oracao-bandas.com/src/database/resources/bands/repositories"
 	"oracao-bandas.com/src/modules/bands/services"
 	"oracao-bandas.com/src/modules/home/controllers"
 )
 
-func InitHomeModule(group *gin.RouterGroup, database *gorm.DB) {
-	bandRepository := repositories.NewPostgresBandRepository(database)
-	bandService := services.NewSaveBandService(bandRepository)
+func InitHomeModule(group *gin.RouterGroup, bandService *services.SaveBandService) {
 	saveBandController := controllers.NewHomeController(bandService)
 	SetupRoutes(group, saveBandController)
 }
