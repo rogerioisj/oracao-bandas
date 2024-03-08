e2145f6146786f28fa819abebf7d1e776ef5ba99
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-03-08 12:57:06 -0300

    refactor: change password range

--- src/database/entities/auth.go
+++ src/database/entities/auth.go
@@ -4,5 +4,5 @@ type Auth struct {
 	BaseEntity
 	Name     string `gorm:"type:varchar(150); NOT_NULL"`
 	Email    string `gorm:"type:varchar(150); unique; NOT_NULL"`
-	Password string `gorm:"type:varchar(100); NOT_NULL"`
+	Password string `gorm:"type:varchar(150); NOT_NULL"`
 }
