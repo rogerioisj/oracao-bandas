a807e9eb58c4923df52c95af67650905f922ef0f
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-05-13 18:13:01 -0300

    refactor: rename file

--- src/database/entities/auth.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-type User struct {
-	BaseEntity
-	Name     string `gorm:"type:varchar(150); NOT_NULL"`
-	Login    string `gorm:"type:varchar(150); unique; NOT_NULL"`
-	Password string `gorm:"type:varchar(150); NOT_NULL"`
-}
