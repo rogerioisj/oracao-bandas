637870f7aa40ac6445848ec9ac5b6d9454ec7b59
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-05-13 20:17:59 -0300

    feat: inject middleware

--- src/modules/auth/routes.go
+++ src/modules/auth/routes.go
@@ -2,13 +2,14 @@ package auth
 
 import (
 	"github.com/gin-gonic/gin"
+	"oracao-bandas.com/src/middlewares"
 	"oracao-bandas.com/src/modules/auth/controllers"
 )
 
-func SetupRoutes(router *gin.RouterGroup, controller controllers.AuthControllerInterface) {
+func SetupRoutes(router *gin.RouterGroup, controller controllers.AuthControllerInterface, middlewares middlewares.SessionCheckInterface) {
 	authGroup := router.Group("auth")
 
-	authGroup.POST("register-user", controller.RegisterUser)
+	authGroup.POST("register-user", middlewares.CheckSessionMiddleware(), controller.RegisterUser)
 	authGroup.POST("login", controller.Login)
 	authGroup.GET("logout", controller.Logout)
 }
