3051a20c25b06185852538c8e9eca601d934f2ed
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-05-13 11:39:17 -0300

    feat: add button behavior

--- src/static/index.js
+++ src/static/index.js
@@ -34,3 +34,15 @@ openRegisterModalButton.addEventListener('click', () => {
 closeRegisterModalButton.addEventListener('click', () => {
     registerModal.close();
 });
+
+const openUserModalButton = document.querySelector('#open-user-modal');
+const UserModal = document.querySelector('#user-modal');
+const closeUserModalButton = document.querySelector('#close-user-modal');
+
+openUserModalButton.addEventListener('click', () => {
+    UserModal.showModal();
+});
+
+closeUserModalButton.addEventListener('click', () => {
+    UserModal.close();
+});
