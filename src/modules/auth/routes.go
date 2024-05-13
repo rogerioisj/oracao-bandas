6a5d6c1b342e62b1f668c162aa327b400a3f7ee8
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-05-13 14:31:56 -0300

    feat: add routes

---
+++ src/modules/auth/routes.go
@@ -0,0 +1,13 @@
+package auth
+
+import (
+	"github.com/gin-gonic/gin"
+	"oracao-bandas.com/src/modules/auth/controllers"
+)
+
+func SetupRoutes(router *gin.RouterGroup, controller controllers.AuthControllerInterface) {
+	authGroup := router.Group("auth")
+
+	authGroup.POST("register-user", controller.RegisterUser)
+	authGroup.POST("login", controller.Login)
+}
