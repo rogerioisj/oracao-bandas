619daa0e87fde69f25c0baf17c4b24871d578746
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-09-15 05:25:25 -0300

    feat: add update method in repository interface

--- src/database/repositories/userRepositoryInterface.go
+++ src/database/repositories/userRepositoryInterface.go
@@ -7,5 +7,6 @@ import (
 type UserRepositoryInterface interface {
 	SearchByLogin(login string) (entities.User, error)
 	Register(name, login, password string) error
+	Update(originLogin, name, login, password string) error
 	List(page, number int, name string) ([]entities.User, int, error)
 }
