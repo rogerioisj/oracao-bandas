414f2403d55a1e284be766d94ed10a6d729de878
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-09-27 02:15:22 -0300

    fix: modal behavior

--- src/static/logged-resources.js
+++ src/static/logged-resources.js
@@ -1,4 +1,5 @@
 const openRegisterModalButton = document.querySelector('#open-band-modal');
+const openMobileRegisterModalButton = document.querySelector('#mobile-open-band-modal');
 const registerModal = document.querySelector('#band-modal');
 const closeRegisterModalButton = document.querySelector('#close-band-modal');
 
@@ -10,11 +11,20 @@ closeRegisterModalButton.addEventListener('click', () => {
     registerModal.close();
 });
 
+openMobileRegisterModalButton.addEventListener('click', () => {
+    registerModal.showModal();
+});
+
 const openUserModalButton = document.querySelector('#open-user-modal');
+const mobileOpenUserModalButton = document.querySelector('#mobile-open-user-modal');
 const UserModal = document.querySelector('#user-modal');
 const closeUserModalButton = document.querySelector('#close-user-modal');
 
 openUserModalButton.addEventListener('click', () => {
+    UserModal.showModal();
+});
+
+mobileOpenUserModalButton.addEventListener('click', () => {
     UserModal.showModal();
 });
 
@@ -23,10 +33,15 @@ closeUserModalButton.addEventListener('click', () => {
 });
 
 const openUpdatedUserModalButton = document.querySelector('#open-update-user-modal');
+const mobileOpenUpdatedUserModalButton = document.querySelector('#mobile-open-update-user-modal');
 const UpdateUserModal = document.querySelector('#update-user-modal');
 const closeUpdateUserModalButton = document.querySelector('#close-update-user-modal');
 
 openUpdatedUserModalButton.addEventListener('click', () => {
+    UpdateUserModal.showModal();
+});
+
+mobileOpenUpdatedUserModalButton.addEventListener('click', () => {
     UpdateUserModal.showModal();
 });
 
