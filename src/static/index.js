af466a13e9e68820c320aa7716b1104f6d1a36eb
Rogerio Inacio <rogerio.reaper@gmail.com>
2024-09-27 02:02:35 -0300

    feat: add lateral menu behavior

--- src/static/index.js
+++ src/static/index.js
@@ -1,8 +1,8 @@
-/*NAV BAR */
-
 const currentPath = window.location.pathname;
 const homeLink = document.getElementById('home-link');
+const mobileHomeLink = document.getElementById('mobile-home-link');
 const userLink = document.getElementById('users-link');
+const mobileUserLink = document.getElementById('mobile-users-link');
 
 console.log("PATH: ", currentPath)
 console.log("HOME: ", homeLink)
@@ -12,6 +12,12 @@ if (currentPath === '/' && homeLink) {
     homeLink.classList.add('border-r-4');
     homeLink.classList.add('border-red-600');
     homeLink.classList.remove('border-white');
+}
+
+if (currentPath === '/' && mobileHomeLink) {
+    mobileHomeLink.classList.add('border-r-4');
+    mobileHomeLink.classList.add('border-red-600');
+    mobileHomeLink.classList.remove('border-white');
 }
 
 if (currentPath === '/' && userLink && homeLink) {
@@ -24,6 +30,16 @@ if (currentPath === '/' && userLink && homeLink) {
     userLink.classList.add('border-white');
 }
 
+if (currentPath === '/' && mobileUserLink && mobileHomeLink) {
+    mobileHomeLink.classList.add('border-r-4');
+    mobileHomeLink.classList.add('border-red-600');
+    mobileHomeLink.classList.remove('border-white');
+
+    mobileUserLink.classList.remove('border-r-4');
+    mobileUserLink.classList.remove('border-red-600');
+    mobileUserLink.classList.add('border-white');
+}
+
 if (currentPath !== '/' && userLink && homeLink) {
     userLink.classList.add('border-r-4');
     userLink.classList.add('border-red-600');
@@ -34,18 +50,15 @@ if (currentPath !== '/' && userLink && homeLink) {
     homeLink.classList.add('border-white');
 }
 
-const burger = document.querySelector('#burger');
-const menu = document.querySelector('#menu');
+if (currentPath !== '/' && mobileUserLink && mobileHomeLink) {
+    mobileUserLink.classList.add('border-r-4');
+    mobileUserLink.classList.add('border-red-600');
+    mobileUserLink.classList.remove('border-white');
 
-const showMenu = () => {
-    if (menu.classList.contains('hidden')) {
-        menu.classList.remove('hidden');
-    } else {
-        menu.classList.add('hidden');
-    }
-};
-
-burger.addEventListener('click', showMenu);
+    mobileHomeLink.classList.remove('border-r-4');
+    mobileHomeLink.classList.remove('border-red-600');
+    mobileHomeLink.classList.add('border-white');
+}
 
 const openLoginModalButton = document.querySelector('#open-login-modal');
 const loginModal = document.querySelector('#login-modal');
@@ -60,3 +73,42 @@ if (openLoginModalButton !== null) {
         loginModal.close();
     });
 }
+
+const openMobileLoginModalButton = document.querySelector('#mobile-open-login-modal');
+const mobileLoginModal = document.querySelector('#login-modal');
+const closeMobileLoginModalButton = document.querySelector('#close-login-modal');
+
+if (openMobileLoginModalButton !== null) {
+    openMobileLoginModalButton.addEventListener('click', () => {
+        mobileLoginModal.showModal();
+    });
+
+    closeMobileLoginModalButton.addEventListener('click', () => {
+        mobileLoginModal.close();
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.getElementById('burger');
+    const mobileMenu = document.getElementById('mobile-menu');
+
+    const toggleMobileMenu = () => {
+        mobileMenu.classList.toggle('translate-x-full');
+        mobileMenu.classList.toggle('translate-x-0');
+    };
+
+    burger.addEventListener('click', (event) => {
+        event.stopPropagation();
+        toggleMobileMenu();
+    });
+
+    document.addEventListener('click', (event) => {
+        const isClickInsideMenu = mobileMenu.contains(event.target);
+        const isClickOnBurger = burger.contains(event.target);
+
+        if (!mobileMenu.classList.contains('translate-x-full') && !isClickInsideMenu && !isClickOnBurger) {
+            mobileMenu.classList.add('translate-x-full');
+            mobileMenu.classList.remove('translate-x-0');
+        }
+    });
+});
